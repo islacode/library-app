1f4a20ebd72a9db9fd4be02668c4481af039eb43
Isla the Witch <isla.witch.morrison@gmail.com>
2025-03-30 11:27:54 +0200

    added basic pages and routing

--- src/components/MainNav/MainNav.tsx
+++ src/components/MainNav/MainNav.tsx
@@ -1,8 +1,7 @@
-'use client'
+'use client';
 
 import { usePathname } from 'next/navigation';
-import Link from "next/link";
-
+import Link from 'next/link';
 
 export interface NavItem {
   id: number;
@@ -11,27 +10,29 @@ export interface NavItem {
 }
 
 const navItems: NavItem[] = [
-  { id: 1, name: "Home", path: '/' },
-  { id: 2, name: "Books", path: '/books' },
-]
+  { id: 1, name: 'Home', path: '/' },
+  { id: 2, name: 'Books', path: '/books' },
+];
 
 export default function MainNav() {
   const pathname = usePathname();
 
   return (
     <nav>
-      <ul className="list-none flex gap-4">
+      <ul className="flex list-none gap-4">
         {navItems.map((navItem) => {
           const hover = pathname === navItem.path ? '' : 'hover:';
           const className = `p-2 ${hover}bg-gray-500 rounded-lg`;
 
           return (
             <li key={navItem.id}>
-              <Link className={className} href={navItem.path}>{navItem.name}</Link>
+              <Link className={className} href={navItem.path}>
+                {navItem.name}
+              </Link>
             </li>
-          )
+          );
         })}
       </ul>
     </nav>
-  )
+  );
 }
